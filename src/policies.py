577121f7957e1b8d7c46c485f7286b93df7e2931
Richard <wads01@gmail.com>
2022-06-29 17:47:54 +0100

    add flattened softmax policy

--- src/policies.py
+++ src/policies.py
@@ -67,3 +67,33 @@ class SoftmaxDirectionalPolicy(Policy):
     def get_action(self, T):     
         return lambda q,s: self.action(q, s, T=T)
 
+
+class SoftmaxFlattenedPolicy(Policy):
+    
+    def __init__(self, env, rng):
+        super().__init__(env)
+        self.rng = rng
+
+
+    def action(self, q, observation, T):
+
+        #get average action state values across all possible actions.  i.e. get a 2d slice of the 3d matrix
+        q_mean = np.mean(q, axis=(0))
+
+        probs = np.exp(q_mean[observation[0]]/T) / np.sum(np.exp(q_mean[observation[0]]/T))
+        probs =  probs/ np.sum(probs) # Ensure probs is normalised to 1 (to avoid rounding errors)
+        randchoice = self.rng.random()
+        flag = 1; k = 1
+        while flag:
+
+            if randchoice<np.sum(probs[0:k]):
+                action = k-1 # adjust for zero based action index
+                flag = 0
+            
+            k = k + 1
+
+        return action
+
+    def get_action(self, T):     
+        return lambda q,s: self.action(q, s, T=T)
+
