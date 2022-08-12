43b3824413c8cdfb98e2deb741d648dd59f63dee
Richard <wads01@gmail.com>
2022-08-12 16:44:16 +0100

    add tests for manhattan distance

--- src/manhattan.py
+++ src/manhattan.py
@@ -1,3 +1,4 @@
+from utils import get_sliding_window_sequence
 from utils import map_index_to_coord
 import numpy as np
 
@@ -29,5 +30,25 @@ def get_manhattan_similarity(size, route_index1, route_index2):
     
     # add the difference in route length
     total_distance += (len(longest_route) - len(shortest_route))
+    
+    return total_distance
+
+
+def get_manhattan_distance(size, route):
+    
+    
+    coords = [map_index_to_coord(size,x) for x in route]
+    
+    sliding_window_sequence = get_sliding_window_sequence(2, len(route))
+
+    total_distance = 0
+
+    for window in sliding_window_sequence:
+        
+        x_p, y_p = coords[window[0]] # x, y co-ordinate of p
+        x_q, y_q = coords[window[1]] # x, y co-ordinate of q
+
+        #calculate the l1 distance between each coordinate pair
+        total_distance += np.abs(x_p - x_q) + np.abs(y_p - y_q)  
     
     return total_distance
--- src/test_manhattan.py
+++ src/test_manhattan.py
@@ -1,36 +1,55 @@
 import unittest
-from manhattan import get_manhattan_similarity
+from manhattan import get_manhattan_similarity, get_manhattan_distance
 
 class Test_Map(unittest.TestCase):
 
-    def test_manhattan_distance_different_length(self):
+    def test_manhattan_similarity_different_length(self):
 
         size =4
         route_1_index = [0,1,5,6]
         route_2_index = [0,4,8,9,5,6]
 
-        distance = get_manhattan_similarity(size, route_1_index, route_2_index)
+        similarity = get_manhattan_similarity(size, route_1_index, route_2_index)
         
-        self.assertEqual(distance, 8)
+        self.assertEqual(similarity, 8)
 
-    def test_manhattan_distance_same_length(self):
+    def test_manhattan_similarity_same_length(self):
 
         size =4
         route_1_index = [0,4,8,9,5,6]
         route_2_index = [0,4,8,9,5,6]
 
-        distance = get_manhattan_similarity(size, route_1_index, route_2_index)
+        similarity = get_manhattan_similarity(size, route_1_index, route_2_index)
         
-        self.assertEqual(distance, 0)
+        self.assertEqual(similarity, 0)
 
-    def test_manhattan_distance_different_route_same_length(self):
+    def test_manhattan_similarity_different_route_same_length(self):
 
         size =4
         route_1_index = [0,1,5,6,10,11,15]
         route_2_index = [0,4,5,6,10,11,15]
 
-        distance = get_manhattan_similarity(size, route_1_index, route_2_index)
+        similarity = get_manhattan_similarity(size, route_1_index, route_2_index)
         
-        self.assertEqual(distance, 2)
+        self.assertEqual(similarity, 2)
+
+class Test_Distance(unittest.TestCase):
+
+    def test_get_manhattan_distance(self):
+
+            size =4
+            route = [0,1,5,6,10,11,15]
+            distance = get_manhattan_distance(size, route)
+            self.assertEqual(distance, 6)
+
+            route = [0,6,13]
+            distance = get_manhattan_distance(size, route)
+            self.assertEqual(distance, 6)
+
+            route = [10,4,7,12]
+            distance = get_manhattan_distance(size, route)
+            self.assertEqual(distance, 11)
+            
+
 
 unittest.main('test_manhattan', argv=[''], verbosity=2, exit=False)
